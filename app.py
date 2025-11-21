13ba152b09f1e6a868280be813bd36a3faffb3ac
z3n0n69 <binarycipher6@gmail.com>
2025-11-21 15:23:25 +0800

    login functionable. no database authentication yet

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, redirect, render_template 
-
+from flask import Flask, redirect, render_template, request, jsonify
+from flask_socketio import SocketIO, send
 app = Flask('__name__')
+socketio = SocketIO(app)
+
+
+
+#===================
+# GET REQUEST
+#===================
 
 
 @app.route('/')
@@ -8,5 +15,32 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__': 
-    app.run(debug=True, host = '0.0.0.0')
+
+@app.route('/dashboard')    
+def dashboard():
+    return render_template('dashboard.html')
+
+
+
+
+
+#===================
+# POST REQUEST
+#===================
+
+#This will validate the login
+@socketio.on("login")
+
+def loginValidation(info): 
+    print("received something from the server")
+    print(f"Recevied: {type(info)}")
+    usernameInput = info.get("username")
+    passwordInput = info.get("password") 
+    #authenticate in the database
+    print(f"Data: \nUsername = {usernameInput}\nPassword = {passwordInput}")
+    send(f"authenticated" , broadcast = True); 
+
+    
+
+if __name__ == '__main__':
+    socketio.run(app, debug = True , host="0.0.0.0")
--- static/index.js
+++ static/index.js
@@ -1,13 +1,30 @@
 let form = document.getElementById("loginForm"); 
 form.addEventListener('submit', function(event){
     event.preventDefault();
+    //gets the value from the form then send it to the server 
     let username = document.forms["loginForm"]["usernameinput"].value; 
     let password = document.forms["loginForm"]["passwordinput"].value; 
     if(username == "" || password == ""){
         window.alert("Enter something bitch")
     }
     else {
+        //using socketio to have real-time updates with the server
+        let loginInformation = {
+            username:username,
+            password:password
+        }
 
+        const socket = io("http://localhost:5000"); 
+        socket.on("connect", () => {
+
+            socket.emit("login", loginInformation);
+            
+        }); 
+        socket.on("message", (msg) => {
+            console.log("Communication from server: ", msg)
+        });
     } 
 });
-s
+
+
+
